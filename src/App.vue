cd2a19659240212b8f52f5f825310f8434ef9869
breezea <80466674+breezea@users.noreply.github.com>
2024-04-26 09:48:18 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-  import { useMouse } from '@vueuse/core'
-
-  const {x,y} = useMouse()
+import { useMouse } from '@vueuse/core'
+console.log("hello world")
+const {x,y} = useMouse()
 
 </script>
 
